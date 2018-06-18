d643c387f9e6fbeaa2be286cf1392ac211abf8fd
Bruno Gontijo Batista <lotusavatar@gmail.com>
2018-06-18 15:15:32 -0300

    Creating project

---
+++ BattleTank_U4/Source/BattleTank_U4/BattleTank_U4.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "BattleTank_U4.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, BattleTank_U4, "BattleTank_U4" );
---
+++ BattleTank_U4/Source/BattleTank_U4/BattleTank_U4GameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "BattleTank_U4GameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BATTLETANK_U4_API ABattleTank_U4GameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
